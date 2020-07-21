9a219647531118d11291e83adccc9a7b382590ba
Jessye Ejdelman <jessye.ejdelman@gmail.com>
2020-07-21 17:07:45 -0400

    Add user new spec test cannot create a user with an email that is already in the system

--- spec/features/users/new_spec.rb
+++ spec/features/users/new_spec.rb
@@ -59,4 +59,27 @@ RSpec.describe 'new user page', type: :feature do
 
     expect(page).to have_content("Email can't be blank")
   end
+  it 'cannot create a user with an email that is already in the system' do
+    user_1 = User.create(name: 'Neeru Ericsson', address: '33 Cherry St', city: 'Denvor', state: 'CA', zip: '12346', email: 'neeru_is_cool@turing.io', password: 'test123')
+
+    visit '/register'
+
+    password = "test"
+
+    within '.registration_form' do
+      fill_in 'Name', with: 'Neeru Eric'
+      fill_in 'Address', with: '33 Chery St'
+      fill_in 'City', with: 'Denver'
+      fill_in 'State', with: 'CO'
+      fill_in 'Zip', with: '12345'
+      fill_in 'Email', with: 'neeru_is_cool@turing.io'
+      fill_in 'Password', with: password
+      fill_in 'Confirm password', with: password
+      click_on 'Create User'
+    end
+
+    expect(current_path).to eq('/register')
+
+    expect(page).to have_content("Email has already been taken")
+  end
 end
