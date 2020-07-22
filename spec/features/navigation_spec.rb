83274b125a4b24f20a37d24fdc71191c0eb5cd07
Cam C <ckc.cameron@gmail.com>
2020-07-22 16:33:25 -0600

    Write spec for merchant dashboard within merchant nav

--- spec/features/navigation_spec.rb
+++ spec/features/navigation_spec.rb
@@ -31,7 +31,29 @@ RSpec.describe 'Site Navigation' do
       within 'nav' do
         expect(page).to have_content("Cart: 0")
       end
+    end
+  end
 
+  describe 'As a merchant employee' do
+    it "displays nav bar with same links as a regular user including a link to merchant's dashboard" do
+      merchant = User.create(name: 'Ross Geller',
+                            address: '33 Banana St',
+                            city: 'New York',
+                            state: 'NY',
+                            zip: '12345',
+                            email: 'dinosaurs_are_cool@turing.io',
+                            password: 'test124',
+                            role: 1)
+
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(merchant)
+
+      visit '/merchants'
+
+      within 'nav' do
+        click_link 'Dashboard'
+      end
+
+      expect(current_path).to eq('/merchant')
     end
   end
 end
