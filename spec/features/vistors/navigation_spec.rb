4ca30053bfe4d5be22c3570491ff27870da8fd5c
Jessye Ejdelman <jessye.ejdelman@gmail.com>
2020-07-22 10:05:00 -0400

    Add visitor navigation spec test - it cannot access merchant, admin, or profile pages

--- spec/features/vistors/navigation_spec.rb
+++ spec/features/vistors/navigation_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe 'Site Navigation' do
   describe 'As a Visitor' do
-    it "I see a nav bar with links to all pages" do
+    it "can see a nav bar with links to all pages" do
       visit '/'
 
       within(".topnav") do
@@ -13,6 +13,18 @@ RSpec.describe 'Site Navigation' do
         expect(page).to have_link("All Items")
         expect(page).to have_link("Cart: 0")
       end
+    end
+    it "cannot access merchant, admin, or profile pages" do
+      visit '/'
+
+      visit '/merchant'
+      expect(page).to have_content("404 Error: Content not found")
+
+      visit '/admin'
+      expect(page).to have_content("404 Error: Content not found")
+
+      visit '/profile'
+      expect(page).to have_content("404 Error: Content not found")
     end
   end
 end
