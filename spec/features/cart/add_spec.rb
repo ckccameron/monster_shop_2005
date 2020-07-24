632e3cb88b77bdbb8d4f6e0d1c0b6b74f1b99d22
Jessye Ejdelman <jessye.ejdelman@gmail.com>
2020-07-23 23:49:28 -0400

    Add test it will remove item from cart if quantity is 0

--- spec/features/cart/add_spec.rb
+++ spec/features/cart/add_spec.rb
@@ -7,6 +7,7 @@ RSpec.describe 'Cart creation' do
       @mike = Merchant.create(name: "Mike's Print Shop", address: '123 Paper Rd.', city: 'Denver', state: 'CO', zip: 80203)
       @paper = @mike.items.create(name: "Lined Paper", description: "Great for writing on!", price: 20, image: "https://cdn.vertex42.com/WordTemplates/images/printable-lined-paper-wide-ruled.png", inventory: 25)
       @pencil = @mike.items.create(name: "Yellow Pencil", description: "You can write on paper with it!", price: 2, image: "https://images-na.ssl-images-amazon.com/images/I/31BlVr01izL._SX425_.jpg", inventory: 100)
+      @mug = @mike.items.create(name: "Mug", description: "This mug is ugly and chipped", price: 2, image: "https://images-na.ssl-images-amazon.com/images/I/31BlVr01izL._SX425_.jpg", inventory: 3)
     end
 
     it "I see a link to add this item to my cart" do
@@ -67,9 +68,7 @@ RSpec.describe 'Cart creation' do
       expect(page).to have_content("9")
     end
     it "I can increment the count of items I want to purchase unless it is more than the inventory" do
-      mug = @mike.items.create(name: "Mug", description: "This mug is ugly and chipped", price: 2, image: "https://images-na.ssl-images-amazon.com/images/I/31BlVr01izL._SX425_.jpg", inventory: 3)
-
-      visit "/items/#{mug.id}"
+      visit "/items/#{@mug.id}"
       click_on "Add To Cart"
 
       visit '/cart'
@@ -80,6 +79,18 @@ RSpec.describe 'Cart creation' do
       expect(page).to have_content("3")
       click_link "+"
       expect(page).to have_content("3")
+    end
+    it "If I decrement item to 0 it is removed from my cart" do
+      visit "/items/#{@mug.id}"
+      click_on "Add To Cart"
+
+      visit '/cart'
+      click_link "+"
+      expect(page).to have_content("2")
+      click_link "-"
+      expect(page).to have_content("1")
+      click_link "-"
+      expect(page).to have_content("Cart is currently empty")
     end
   end
 end
