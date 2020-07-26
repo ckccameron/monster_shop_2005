972c62b5c5ecce0570aa48fdcbf4da108ed6a41b
“Neeru <neeram85@gmail.com>
2020-07-26 11:42:55 -0600

    Finish user story 39

--- app/controllers/admin/merchants_controller.rb
+++ app/controllers/admin/merchants_controller.rb
@@ -7,6 +7,7 @@ class Admin::MerchantsController < Admin::BaseController
     merchant = Merchant.find(params[:id])
     merchant.status = "disabled"
     merchant.save
+    merchant.disable_items
     redirect_to "/admin/merchants"
     flash[:message] = "#{merchant.name} is now disabled"
   end
