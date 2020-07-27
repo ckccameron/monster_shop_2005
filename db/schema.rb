22079e4c140a5b1b165da96f97f73a3d8f67687b
Cam C <ckc.cameron@gmail.com>
2020-07-26 21:22:53 -0600

    Run migration to add pending default value to orders status

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200727005858) do
+ActiveRecord::Schema.define(version: 20200727031824) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -58,7 +58,7 @@ ActiveRecord::Schema.define(version: 20200727005858) do
     t.integer "zip"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "status"
+    t.string "status", default: "pending"
     t.bigint "user_id"
     t.index ["user_id"], name: "index_orders_on_user_id"
   end
