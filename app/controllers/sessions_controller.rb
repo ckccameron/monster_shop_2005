a30e9daafac1616a6dd3a21c3d72b37a73bd846d
Jessye Ejdelman <jessye.ejdelman@gmail.com>
2020-07-28 15:23:54 -0400

    Fix admin merchants items path

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,7 +8,6 @@ class SessionsController < ApplicationController
 
   def create
     @user = User.find_by(email: params[:email])
-
     if @user.authenticate(params[:password])
       session[:user_id] = @user.id
       if @user.role == "default"
