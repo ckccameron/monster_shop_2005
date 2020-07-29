37fa19994a8552447619c46d6d5c591d5fcc13a5
Cam C <ckc.cameron@gmail.com>
2020-07-29 15:58:03 -0600

    Add namespaced admin route to update order status

--- config/routes.rb
+++ config/routes.rb
@@ -57,6 +57,7 @@ Rails.application.routes.draw do
   namespace :admin do
     get "/", to: "dashboard#index"
     get "/users", to: "users#index"
+    get "/orders/:order_id", to: "orders#update"
   end
 
   namespace :profile do
