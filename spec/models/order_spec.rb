ca5681a11251e294181dcb6ed9ef002205e85fe0
Cam C <ckc.cameron@gmail.com>
2020-07-29 15:49:02 -0600

    Write spec for packaged_orders method for Order model

--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -61,5 +61,31 @@ describe Order, type: :model do
 
       expect(order.status).to eq("packaged")
     end
+
+    it ".packaged_orders" do
+      regular_user = User.create(name: 'Neeru Ericsson', address: '33 Cherry St', city: 'Denver', state: 'CO', zip: '12346', email: 'neeru_is_cool@turing.io', password: 'test123', role: 0)
+
+      meg = Merchant.create(name: "Meg's Bike Shop", address: '123 Bike Rd.', city: 'Denver', state: 'CO', zip: 80203)
+      brian = Merchant.create(name: "Brian's Dog Shop", address: '125 Doggo St.', city: 'Denver', state: 'CO', zip: 80210)
+
+      helmet = meg.items.create(name: "Brain Cage", description: "A cage to protect your head", price: 150, image: "https://www.rei.com/media/4e1f5b05-27ef-4267-bb9a-14e35935f218?size=784x588", inventory: 70)
+      catnip = brian.items.create(name: "Catnip", description: "It'll get your cat super high", price: 10, image: "http://lovencaretoys.com/image/cache/dog/tug-toy-dog-pull-9010_2-800x800.jpg", inventory: 50)
+      scratch_pad = brian.items.create(name: "Scratch Pad", description: "Pretty scratchy", price: 10, image: "http://lovencaretoys.com/image/cache/dog/tug-toy-dog-pull-9010_2-800x800.jpg", inventory: 5)
+      dog_bone = brian.items.create(name: "Dog Bone", description: "They'll love it!", price: 21, image: "https://img.chewy.com/is/image/catalog/54226_MAIN._AC_SL1500_V1534449573_.jpg", active?:false, inventory: 21)
+
+      order_1 = regular_user.orders.create!(name: "Neeru Ericsson", address: "33 Cherry St", city: "Denver", state: "CO", zip: "12346", status: 0)
+      order_1.item_orders.create(order: order_1, item: catnip, price: catnip.price, quantity: 2)
+
+      order_2 = regular_user.orders.create!(name: "Neeru Ericsson", address: "33 Cherry St", city: "Denver", state: "CO", zip: "12346", status: 3)
+      order_2.item_orders.create(order: order_2, item: scratch_pad, price: scratch_pad.price, quantity: 2)
+
+      order_3 = regular_user.orders.create!(name: "Neeru Ericsson", address: "33 Cherry St", city: "Denver", state: "CO", zip: "12346", status: 1)
+      order_3.item_orders.create(order: order_3, item: dog_bone, price: dog_bone.price, quantity: 2)
+
+      order_4 = regular_user.orders.create!(name: "Neeru Ericsson", address: "33 Cherry St", city: "Denver", state: "CO", zip: "12346", status: 0)
+      order_4.item_orders.create(order: order_4, item: dog_bone, price: dog_bone.price, quantity: 2)
+
+      expect(Order.packaged_orders).to eq([order_1, order_4])
+    end
   end
 end
